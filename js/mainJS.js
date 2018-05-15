be291f3124525c689cffc1d6dda69d7cbf9a8ef2
yetinbmf <yatendra.bhardwaj@tothenew.com>
2018-05-15 17:14:53 +0530

    Banner Ui improvements

--- js/mainJS.js
+++ js/mainJS.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
     $(window).bind('scroll', function() {
-    var navHeight = $( window ).height() - 70;
+        var navHeight = $( window ).height() - 70;
             if ($(window).scrollTop() > navHeight) {
                 $('body').addClass('fixed-header');
             }
             else {
                 $('body').removeClass('fixed-header');
             }
-        });
+    });
+    // $(".slide-bottom").click(function(){
+    //     $("#top-section-one").slideDown("slow");
+    // });
+
 });
 
